6a621f1118f9d21eb3232265379cbfe0c3a19f66
srinu-vinay-kumar <srinuvinaykumar.narigiri@gmail.com>
2026-06-23 08:34:07 +0530

    First Commit

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,10 @@ const app = express();
 // * middlewares
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: [
+      "http://localhost:5173",
+      "https://ai-travel-planner-pi-brown.vercel.app/",
+    ],
     credentials: true,
   }),
 );
